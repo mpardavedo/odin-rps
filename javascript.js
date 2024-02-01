8e9cb184acff7f216f2e50ec5221c4f893f14946
MPardave <pardave.domansich@hotmail.com>
2024-02-01 12:18:32 -0500

    ui finished

--- javascript.js
+++ javascript.js
@@ -1,58 +1,51 @@
-const elements = ['ROCK', 'PAPER', 'SCISSORS'];
+let pj;
+let pjScore = 0;
+let pc;
+let pcScore = 0;
+let gameEnded = false;
 
-function computerChoice () {
-    let random = Math.floor(Math.random() * elements.length);
-    return elements[random];
+let choices = ['rock', 'paper', 'scissors'];
+
+window.onload = function () {
+    for (let i = 0; i < 3; i++) {
+        let choice = document.createElement('img');
+        choice.id = choices[i];
+        choice.src = choices[i] + '.png';
+        choice.addEventListener('click', selectChoice);
+        document.getElementById('choices').append(choice);
+    }
 }
 
-function userChoice () {
-    const user = prompt('RPS?');
-    return user.toUpperCase()
-}
+function selectChoice() {
+    if (gameEnded) return;
+    pj = this.id;
+    document.getElementById('pj-choice').src = pj + '.png';
 
-function round (userChoice, computerChoice) {
-    if( userChoice === 'ROCK' && computerChoice === 'SCISSORS' || 
-    userChoice === 'PAPER' && computerChoice === 'ROCK' || 
-    userChoice === 'SCISSORS' && computerChoice === 'PAPER') {
-        return 'win';
-    } else if ( userChoice === computerChoice) {
-        return 'draw';
-    }   
-    return 'lose';
-}
+    pc = choices[Math.floor(Math.random() * 3)];
+    document.getElementById('pc-choice').src = pc + '.png';
 
-function game () {
-    let userScore = 0;
-    let cpuScore = 0;
-
-    do {
-        let user = userChoice();
-        let cpu = computerChoice();
-        let result = round (user,cpu);
-
-        if (result === 'win') {
-            userScore++;
-            alert('You win the round!');
-        } else if (result === 'lose') {
-            cpuScore++;
-            alert('You lose the round');
+    let message = document.querySelector('.message');
+    if (pj == pc) {
+        message.textContent='Its a draw, rematch!';
+    }
+    else {
+        if (pj == 'rock' && pc == 'scissors'|| pj == 'paper' && pc == 'rock' || pj == 'scissors' && pc == 'paper') {
+            pjScore++;
+            message.textContent='You win the round!';
         } else {
-            alert('Draw');
+            pcScore++;
+            message.textContent='You lose the round!';
         }
+    }
 
-        alert ('User: ' + userScore + ' | CPU: ' + cpuScore);
+    document.getElementById('pj-score').innerText = pjScore;
+    document.getElementById('pc-score').innerText = pcScore;
 
-
-        if (userScore === 4) {
-            return alert ('You won the game!');
-        } else if (cpuScore === 4) {
-            return alert ('Game over');
-        } 
-        
-    } while (userScore < 4 || cpuScore < 4);
+    if ( pjScore > 2 ) {
+        message.textContent='You win the game!';
+        gameEnded = true;
+    } else if ( pcScore > 2 ) {
+        message.textContent='You lose the game!';
+        gameEnded = true;
+    }
 }
-
-
-game();
-
-
